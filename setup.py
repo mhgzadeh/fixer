4e77760bc0850dd8de15ea058f16fb58c1bf9bc3
Mohammad Hosseingholizadeh <m.hgzahdeh@gmail.com>
2021-08-12 19:40:39 +0430

    init commit: creating fixer packages -- mhgz-210812

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fixer',
+    version='1.0.1',
+    description='fixer service packages: providing important worlds currencies.',
+    url='https://github.com/mhgzadeh/fixer.git',
+    author='Mohammad',
+    author_email='m.hgzadeh@gmail.com',
+    license='MTL',
+    packages=['get_data'],
+    zip_safe=False
+)
