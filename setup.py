cde5f3d8b7470659f38845725ab59a90441f35b3
Mohammad Hosseingholizadeh <m.hgzahdeh@gmail.com>
2021-08-13 18:52:11 +0430

    edit setup file to add send_data package to it

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='fixer',
-    version='1.0.5',
+    version='1.0.7',
     description='fixer service packages: providing important worlds currencies.',
     url='https://github.com/mhgzadeh/fixer',
     author='Mohammad',
     author_email='m.hgzadeh@gmail.com',
     license='MTL',
-    packages=['get_data'],
+    packages=['get_data', 'send_data'],
     install_requires=['requests'],
     zip_safe=False
 )
